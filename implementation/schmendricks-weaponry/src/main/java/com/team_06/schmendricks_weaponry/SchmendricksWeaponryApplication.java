b394e92ac4676872de3dacba3571020a4505c262
Devon Haynes <devon.haynes@comcast.net>
2025-12-04 16:50:50 -0500

    Imported Spring Boot (with Maven) project

---
+++ implementation/schmendricks-weaponry/src/main/java/com/team_06/schmendricks_weaponry/SchmendricksWeaponryApplication.java
@@ -0,0 +1,13 @@
+package com.team_06.schmendricks_weaponry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchmendricksWeaponryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchmendricksWeaponryApplication.class, args);
+	}
+
+}
---
+++ implementation/schmendricks-weaponry/src/test/java/com/team_06/schmendricks_weaponry/SchmendricksWeaponryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_06.schmendricks_weaponry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchmendricksWeaponryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
